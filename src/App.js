d53a6808d35b807b24db34a800453fd53ce96686
Abdul Moiz Rashid <70377003+abdulmoiz-bit@users.noreply.github.com>
2022-09-05 23:54:20 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Header from "./MyComponents/Header";
+//import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
-import { Footer } from "./MyComponents/Footer";
+//import { Footer } from "./MyComponents/Footer";
 import { AddTodo } from "./MyComponents/AddTodo";
-import { About } from "./MyComponents/About";
+//import { About } from "./MyComponents/About";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -15,23 +15,9 @@ function App() {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
 
-  const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo);
-    // Deleting this way in react does not work
-    // let index = todos.indexOf(todo);
-    // todos.splice(index, 1);
-
-    setTodos(
-      todos.filter((e) => {
-        return e !== todo;
-      })
-    );
-    console.log("deleted", todos);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
 
   const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc);
+    //console.log("I am adding this todo", title, desc);
     let sno;
     if (todos.length === 0) {
       sno = 0;
@@ -48,6 +34,23 @@ function App() {
     console.log(myTodo);
   };
 
+
+  const onDelete = (todo) => {
+    //console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    // console.log("deleted", todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+
   const [todos, setTodos] = useState(initTodo);
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -56,7 +59,6 @@ function App() {
   return (
     <>
       <Router>
-        <Header title="My Todos List" searchBar={false} />
         <Switch>
           <Route
             exact
@@ -70,11 +72,7 @@ function App() {
               );
             }}
           ></Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
         </Switch>
-        <Footer />
       </Router>
     </>
   );
--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -17,13 +17,13 @@ export const AddTodo = ({ addTodo }) => {
         }
     }
     return (
-        <div className="container my-3">
-            <h3>Add a Todo</h3>
+        <div className="container mt-3">
+            <h3 style={{textAlign:'center', fontSize:'3rem'}}>Add a Todo</h3>
+            <div className='formdiv'>
             <form onSubmit={submit}>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Todo Title</label>
                     <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" id="title" aria-describedby="emailHelp" />
-
                 </div>
                 <div className="mb-3">
                     <label htmlFor="desc" className="form-label">Todo Description</label>
@@ -31,6 +31,7 @@ export const AddTodo = ({ addTodo }) => {
                 </div>
                 <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
             </form>
+            </div>
         </div>
     )
 }
